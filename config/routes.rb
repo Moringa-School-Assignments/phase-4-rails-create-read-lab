75e1006ca691a2bac47e7befe69bd6115e8ff1ff
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-11-26 02:57:26 +0300

    complete assignments

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :plants, only: [:index, :create, :show]
+  # get 'plants', to: 'plants#index'
+  # get 'plants/name', to: 'plants#name'
+  # get 'plants/image', to: 'plants#image'
+  # get 'plants/price:integer', to: 'plants#price'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
